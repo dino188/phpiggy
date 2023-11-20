a686867f0691adc7d0913285442e4773b2ab4b3c
Dino Curjak <dino.curjak@hotmail.com>
2023-11-20 14:01:59 +0100

    Controller adding #1

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,7 +5,20 @@ declare(strict_types=1);
 namespace Framework;
 
 class App{
+
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+        
+    }
+
     public function run(){
         echo "Aplication is running";
+    }
+
+    public function get(string $path) {
+        $this->router->add('GET',$path);
     }
 }
